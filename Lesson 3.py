b6d19dc3dff0cce5ad496b3c0c029ba2f0437f0a
Samuel Altarac <samuel.altarac@gmail.com>
2017-12-18 16:25:28 -0500

    Lesson 3
    only arrays

---
+++ Lesson 3.py
@@ -0,0 +1,34 @@
+# Objective:
+# - Arrays; store many data points in 1 variable
+# - if, elif, and else logic
+# - While loops
+
+# ---------------------------------------------------- #
+
+# Arrays
+# These are lists of values. The values are stored in square brackets
+# and separated by a comma like this:
+
+b = []
+# This is an empty array stored in the varaible b
+
+a = [1, 2.5, 'cat']
+# This is an array with 3 element.
+# the first element in an array is the zero-th element.
+# if you want to print to the screen the number 1 that is stored in the array, you can refference it like this:
+
+print(a[0])
+# This says print to the screen the 0-th element in array called a.
+
+print(a[2])
+# This says print the element of array a that is in position 2. This happens to be the last position of a and it is the string 'cat'
+
+# You can add more things to an array by using the append() function like this:
+
+a.append('new element')
+# This 'new element' is in position 3. To print it to the screen we do this:
+print(a[3])
+
+# Can you have the user input() 3 peices of information about themselves and store all that info in an array called user then print to the screen this information?
+
+# ---------------------------------------------------- #
